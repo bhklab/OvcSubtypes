ab374bdd4f3aa12ac9861f46666e8cd2dc2582f1
lavkan <lavanya.kannan@gmail.com>
2016-03-15 16:41:29 -0400

    classificationAcrossDatasets - preping up for figures; consensus_classifier.R now reports the amounts of concordance with pure and purest subtypes; getConsensusOvarianSubtypes.R trains on all data (previously done for patients with survival data)

--- reports/consensus_classifier.R
+++ reports/consensus_classifier.R
@@ -1,28 +1,54 @@
 source("getConsensusOvarianSubtypes.R")
 ## This file is produced from classificationAcrossDatasets.Rnw
-load("esets.with.survival.RData")
-
+#load("esets.with.survival.RData")
+load("esets.not.rescaled.classified.RData")
 # rescale per gene
-esets.with.survival.scaled <- lapply(esets.with.survival, function(eset) {
+#   esets.with.survival.scaled <- lapply(esets.with.survival, function(eset) {
+#     exprs(eset) <- t(scale(t(exprs(eset))))
+#     return(eset)
+#   })
+
+esets.scaled <- lapply(esets.not.rescaled.classified, function(eset) {
   exprs(eset) <- t(scale(t(exprs(eset))))
   return(eset)
 })
 
-dataset.names <- names(esets.with.survival.scaled)
-
+# dataset.names <- names(esets.with.survival.scaled)
+dataset.names <- names(esets.scaled)
+                       
 classification.vals <- list()
+sample_ids <- list()
 
 for(dataset.name in dataset.names) {
-  left.out.dataset <- esets.with.survival.scaled[[dataset.name]]
+  # left.out.dataset <- esets.with.survival.scaled[[dataset.name]]
+  left.out.dataset <- esets.scaled[[dataset.name]]
   training.dataset.names <- dataset.names[dataset.names != dataset.name]
   
   consensus.classifier.output <- getConsensusOvarianSubtypes(left.out.dataset, .dataset.names.to.keep = training.dataset.names)
   
-  classification.vals[[dataset.name]] <- consensus.classifier.output$Annotated.eset$Ovarian.subtypes
+  classification.vals[[dataset.name]] <- consensus.classifier.output$Annotated.eset$Ovarian.subtypes %>% as.vector
+  sample_ids[[dataset.name]] <- paste(dataset.name,".",consensus.classifier.output$Annotated.eset %>% exprs %>% colnames, sep="")
 }
 
 # Make some tables
 
-print(table(unlist(lapply(esets.with.survival.scaled, function(eset) eset$Helland.subtypes)), unlist(classification.vals)))
-print(table(unlist(lapply(esets.with.survival.scaled, function(eset) eset$Verhaak.subtypes)), unlist(classification.vals)))
-print(table(unlist(lapply(esets.with.survival.scaled, function(eset) eset$Konecny.subtypes)), unlist(classification.vals)))
+print(table(unlist(lapply(esets.scaled, function(eset) eset$Helland.subtypes)), unlist(classification.vals)))
+print(table(unlist(lapply(esets.scaled, function(eset) eset$Verhaak.subtypes)), unlist(classification.vals)))
+print(table(unlist(lapply(esets.scaled, function(eset) eset$Konecny.subtypes)), unlist(classification.vals)))
+
+# Check if classification.vals matches the prediction for the training set- highly pure subtypes
+
+# Check if classification.vals matches the prediction for the pure subtypes 
+compare_subtypes <- function (sample_id)
+{
+pure_subtype = intersection_pooled.subtypes[sample_id,"Verhaak"]
+predicted = 
+  unlist(classification.vals)[((sample_ids %>% unlist) == sample_id) %>% which]
+predicted_subtype = substr(predicted,start = 1,stop = 3)
+pure_subtype == predicted_subtype
+}
+
+## the following gives the amounts of consensus between the pure subtypes and the purest subtypes respectively
+
+lapply(rownames(intersection_pooled.subtypes), compare_subtypes) %>% unlist %>% table
+lapply(rownames(Filtered_intersection_pooled.subtypes), compare_subtypes) %>% unlist %>% table
--- reports/getConsensusOvarianSubtypes.R
+++ reports/getConsensusOvarianSubtypes.R
@@ -5,19 +5,27 @@ getConsensusOvarianSubtypes <- function(eset, .dataset.names.to.keep=names(esets
   ### Load training data
   print("Loading training data")
   ## This file is produced from classificationAcrossDatasets.Rnw
-  load("esets.with.survival.RData")
-  
+  #load("esets.with.survival.RData")
+  load("esets.not.rescaled.classified.RData")
   # rescale per gene
-  esets.with.survival.scaled <- lapply(esets.with.survival, function(eset) {
+#   esets.with.survival.scaled <- lapply(esets.with.survival, function(eset) {
+#     exprs(eset) <- t(scale(t(exprs(eset))))
+#     return(eset)
+#   })
+  
+  esets.scaled <- lapply(esets.not.rescaled.classified, function(eset) {
     exprs(eset) <- t(scale(t(exprs(eset))))
     return(eset)
   })
   
   dataset.names.to.keep <- .dataset.names.to.keep
   
-  esets.with.survival.scaled <- esets.with.survival.scaled[dataset.names.to.keep]
+  #esets.with.survival.scaled <- esets.with.survival.scaled[dataset.names.to.keep]
+  esets.scaled <- esets.scaled[dataset.names.to.keep]
   
-  esets.survival.scaled.merged <- MetaGx::datasetMerging(esets.with.survival.scaled, method = "intersect")
+  # esets.survival.scaled.merged <- MetaGx::datasetMerging(esets.with.survival.scaled, method = "intersect")
+  esets.scaled.merged <- MetaGx::datasetMerging(esets.scaled, method = "intersect")
+  
   
   subtype.correspondances <- data.frame(Konecny=c("C1_immL", "C2_diffL", "C3_profL", "C4_mescL"),
                                         Verhaak=c("IMR", "DIF", "PRO", "MES"),
@@ -25,12 +33,20 @@ getConsensusOvarianSubtypes <- function(eset, .dataset.names.to.keep=names(esets
   
   # Only keep cases that are concordant for all three classifiers
   
-  cases.to.keep <- match(esets.survival.scaled.merged$Konecny.subtypes, subtype.correspondances$Konecny) ==
-    match(esets.survival.scaled.merged$Verhaak.subtypes, subtype.correspondances$Verhaak) &
-    match(esets.survival.scaled.merged$Verhaak.subtypes, subtype.correspondances$Verhaak) ==
-    match(esets.survival.scaled.merged$Helland.subtypes, subtype.correspondances$Helland)
-  
-  training.dataset <- esets.survival.scaled.merged[,cases.to.keep]
+#   cases.to.keep <- match(esets.survival.scaled.merged$Konecny.subtypes, subtype.correspondances$Konecny) ==
+#     match(esets.survival.scaled.merged$Verhaak.subtypes, subtype.correspondances$Verhaak) &
+#     match(esets.survival.scaled.merged$Verhaak.subtypes, subtype.correspondances$Verhaak) ==
+#     match(esets.survival.scaled.merged$Helland.subtypes, subtype.correspondances$Helland)
+#   
+#   training.dataset <- esets.survival.scaled.merged[,cases.to.keep]
+
+cases.to.keep <- match(esets.scaled.merged$Konecny.subtypes, subtype.correspondances$Konecny) ==
+  match(esets.scaled.merged$Verhaak.subtypes, subtype.correspondances$Verhaak) &
+  match(esets.scaled.merged$Verhaak.subtypes, subtype.correspondances$Verhaak) ==
+  match(esets.scaled.merged$Helland.subtypes, subtype.correspondances$Helland)
+
+#training.dataset <- esets.survival.merged[,cases.to.keep]
+training.dataset <- esets.scaled.merged[,cases.to.keep]
   
   ### Once we are happy with the normalization / removal of discordant cases, this eset should be a package data file.
   
@@ -43,12 +59,11 @@ getConsensusOvarianSubtypes <- function(eset, .dataset.names.to.keep=names(esets
   consensus.global.obj <- list(training.dataset=training.dataset, train.labels=train.labels, eset=eset)
   
   assign("consensus.global.obj", consensus.global.obj, .GlobalEnv)
-   
   pamr.model <- pamr.train(data=list(x=exprs(consensus.global.obj$training.dataset), y=consensus.global.obj$train.labels),
                            gene.subset = intersect(rownames(exprs(consensus.global.obj$training.dataset)), rownames(exprs(consensus.global.obj$eset))))
   #pamr.model <- pamr.train(data=list(x=exprs(training.dataset), y=train.labels),
   #                         gene.subset = intersect(rownames(exprs(training.dataset)), rownames(exprs(eset))))
-  
+
   if(threshold.auto == TRUE) {
     threshold <- pamr.adaptthresh(pamr.model)
   } else {
